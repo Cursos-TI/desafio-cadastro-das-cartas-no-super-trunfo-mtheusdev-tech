5e2b20d4823c4a9ee76b4893514e133f1e04f4d8
Matheus Andrade <matheusandradeprata@gmail.com>
2025-07-25 01:43:21 +0000

    TEMA 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,8 @@
 int main() {
     char co1[10], co2[10]; 
     int tu1, tu2;
-    float pib1, pib2, ar1, ar2, po1, po2;
+    float pib1, pib2, ar1, ar2, po1, po2, dp1, dp2;
+    double pc1, pc2;
     char n1[50], n2[50], es1, es2;
 
     printf("Qual o Estado? \n");
@@ -31,6 +32,10 @@ int main() {
     printf("Número de pontos turístico? \n");
     scanf("%d", &tu1);
 
+    dp1 = po1 / ar1;
+    pib1 = pib1 * 1000000000;
+    pc1 = (double)pib1 / po1;
+
      printf("\n--- Carta 01: ---\n");
       printf("Estado: %c \n", es1);
        printf("Código: %s\n", co1);
@@ -39,6 +44,8 @@ int main() {
           printf("Área: %.2f Km² \n", ar1);
            printf("PIB: %.2f bilhões \n", pib1);
             printf("Número de Pontos Turístico: %d \n", tu1);
+             printf("Densidade Populacional: %.2f hab/km²\n", dp1);
+              printf("PIB per Capita: R$ %.2f \n", pc1);
     
             
     printf("\nQual o Estado? \n");
@@ -62,6 +69,10 @@ int main() {
     printf("Número de Pontos Turístico? \n");
     scanf("%d", &tu2);
 
+     dp2 = po2 / ar2;
+     pib2 = pib2 * 1000000000;
+     pc2 = (double) pib2 / po2;
+
      printf("\n--- Carta 02: ---\n");
       printf("Estado: %c \n", es2);
        printf("Código: %s \n", co2);
@@ -70,6 +81,8 @@ int main() {
           printf("Área: %.2f Km² \n", ar2);
            printf("PIB: %.2f bilhões \n", pib2);
             printf("Número de Pontos Turístico: %d \n", tu2);
+             printf("Densidade Populacional: %.2f hab/km² \n", dp2);
+              printf("PIB per Capita: R$ %.2f \n", pc2);
 
     return 0;
 }
