0f10454c176f8ab8f1920b0f6c70152c314befb5
Matheus Andrade <matheusandradeprata@gmail.com>
2025-07-28 23:12:58 +0000

    LÓGICA 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -44,7 +44,7 @@ int main() {
         printf("População: %u \n", po1);
         printf("Área: %.2f Km² \n", ar1);
         printf("PIB: %.2f bilhões \n", pib1);
-        printf("Número de Pontos Turístico: %f \n", tu1);
+        printf("Número de Pontos Turístico: %.2f \n", tu1);
         printf("Densidade Populacional: %.2f hab/km²\n", dp1);
         printf("PIB per Capita: R$ %.2f \n", pc1);
     
@@ -72,7 +72,7 @@ int main() {
 
      dp2 = po2 / ar2;
      pib2 = pib2 * 1000000000;
-     pc2 = (double) pib2 / po2;
+     pc2 = (double) pib2 / po2; 
 
          printf("\n--- Carta 02: ---\n");
          printf("Estado: %c \n", es2);
@@ -81,7 +81,7 @@ int main() {
          printf("População: %u \n", po2);
          printf("Área: %.2f Km² \n", ar2);
          printf("PIB: %.2f bilhões \n", pib2);
-         printf("Número de Pontos Turístico: %f \n", tu2);
+         printf("Número de Pontos Turístico: %.2f \n", tu2);
          printf("Densidade Populacional: %.2f hab/km² \n", dp2);
          printf("PIB per Capita: R$ %.2f \n", pc2);
 
@@ -94,6 +94,42 @@ int main() {
     unsigned long int super1 =(double) po1 + ar1 + pib1  + tu1 + invDap1 + pc1;
     unsigned long int super2 =(double) po2 + ar2 + pib2  + tu2 + invDap2 + pc2;
 
+    // DESENVOLVENDO A LÓGICA DO JOGO
+
+     printf("\n---RESULTADOS---\n");
+
+    if (po1 > po2) {
+        printf("População: %s vence!\n", co1);
+    } else {
+        printf("População: %s vence!\n", co2);
+    }
+    if (ar1 > ar2) {
+        printf("Área: %s vence! \n", co1);
+    } else {
+        printf("Área: %s vence! \n", co2);
+    }
+    if (pib1 > pib2) {
+        printf("PIB: %s vence! \n", co1);
+    } else {
+        printf("PIB: %s vence1 \n", co2);
+    }
+    if (tu1 > tu2) {
+        printf("Turísmo: %s vence! \n", co1);
+    } else {
+        printf("Turísmo: %s vence! \n", co2);
+    }
+    if (pc1 < pc2) {
+        printf("PIB per capita: %s vence! \n", co1);
+    } else {
+        printf("PIB per capita: %s vence! \n", co2);
+    }
+    if (super1 > super2) {
+        printf("Super Poder: %s vence! \n", co1);
+    } else {
+        printf("Super Poder: %s vence! \n", co2);
+    }
+    
+     /* NÍVEL 1 DO MODO MESTRE
     int Vencepop = (po1 > po2) ? 1 : 0;
     int Vencear = (ar1 > ar2) ? 1 : 0;
     double Vencepib = (pib1 > pib2) ? 1 : 0;
@@ -107,10 +143,10 @@ int main() {
      printf("PIB: %u vence!\n", Vencepib? 1 : 2);
      printf("Turísmo: %d vence!\n", Vencenum? 1: 2);
      printf("PIB per capita: %u vence!\n", Venceper? 1 : 2);
-     printf("Super Poder: %u vence!\n", Vencesup? 1 : 2);
+     printf("Super Poder: %u vence!\n", Vencesup? 1 : 2); */
 
      printf("\n---Valores óbitidos: SUPER PODER: %lu---\n", super1);
-     printf("\n---Valores óbitidos: SUPER PODER: %lu---\n", super2);
+     printf("\n---Valores óbitidos: SUPER PODER: %lu---\n", super2); 
 
      printf("\n¨¨¨¨¨ OBRIGADO POR JOGAR!!! ¨¨¨¨¨\n");
     return 0;
